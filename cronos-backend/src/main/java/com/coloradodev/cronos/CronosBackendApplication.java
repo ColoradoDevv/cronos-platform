7556ad47eda2d29f87b18ce122fa9494953e9cf9
Null_v1x <74389451+nullv1x@users.noreply.github.com>
2025-12-05 17:06:02 -0500

    Fix: Backend configuration (Main class & properties template)

---
+++ cronos-backend/src/main/java/com/coloradodev/cronos/CronosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coloradodev.cronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronosBackendApplication.class, args);
+    }
+
+}
